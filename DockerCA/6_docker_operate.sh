ba667d5c1c7dad229c6820bf557827cd5bcf5001
lijiao <lijiao@localhost.localdomain>
2015-10-19 11:39:40 +0800

    a

--- DockerCA/6_docker_operate.sh
+++ DockerCA/6_docker_operate.sh
@@ -8,6 +8,6 @@ fi
 Host=$1
 shift 1
 
-docker --tlsverify --tlscacert=./CA/ca.pem --tlscert=./Cert2/cert2-cert.pem --tlskey=./Cert2/cert2-key.pem -H=$Host:2376 $@
+docker --tlsverify --tlscacert=./CA/ca.pem --tlscert=./Cert2/cert2-cert.pem --tlskey=./Cert2/cert2-key.pem -H=$Host:2375 $@
 #docker --tlsverify --tlscacert=./CA/ca.pem --tlscert=./Cert1/cert1-cert.pem --tlskey=./Cert1/cert1-key.pem -H=$Host:2376 $@
 
